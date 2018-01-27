2dd82fd83a3580fcb3e18eab3b8de7a3b2ad59d2
[사용자 <internetot@naver.com>
2018-01-28 06:41:19 +0900

    django admin 커스트마이징

--- tk/images/models.py
+++ tk/images/models.py
@@ -19,6 +19,9 @@ class Image(TimeStampedModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
 
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
 class Comment(TimeStampedModel):
 
     """Comment Model"""
@@ -27,11 +30,17 @@ class Comment(TimeStampedModel):
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
 
+    def __str__(self):
+        return self.message
+
 class Like(TimeStampedModel):
 
     """ Like Model """
 
     creator = models.ForeignKey(user_models.User, null=True)
     image = models.ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return 'User: {} -  Image Caption: {}'.format(self.creator.username, self.image.caption)
 
 
